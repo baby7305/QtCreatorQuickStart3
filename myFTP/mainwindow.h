b115af94f8a22b23af787950b1868b8efc7ba54b
baby7305 <songhaibei7305@163.com>
2020-11-20 15:30:42 +0800

    FTP

--- myFTP/mainwindow.h
+++ myFTP/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 class QFtp;
+#include <QHash>
+class QFile;
+class QUrlInfo;
+class QTreeWidgetItem;
 
 namespace Ui {
 class MainWindow;
@@ -19,11 +23,27 @@ public:
 private:
     Ui::MainWindow *ui;
     QFtp *ftp;
+    // 用来存储一个路径是否为目录的信息
+    QHash<QString, bool> isDirectory;
+    // 用来存储当前路径
+    QString currentPath;
+    // 用来表示下载的文件
+    QFile *file;
 
 private slots:
     void ftpCommandStarted(int);
     void ftpCommandFinished(int, bool);
 
+    // 更新进度条
+    void updateDataTransferProgress(qint64, qint64 );
+    // 将服务器上的文件添加到Tree Widget部件中
+    void addToList(const QUrlInfo &urlInfo);
+    // 双击一个目录时显示其内容
+    void processItem(QTreeWidgetItem*, int);
+
+    void on_connectButton_clicked();
+    void on_cdToParentButton_clicked();
+    void on_downloadButton_clicked();
 };
 
 #endif // MAINWINDOW_H
